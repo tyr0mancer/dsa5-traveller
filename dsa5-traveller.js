e132893d1d807a7a47b8d9fa8ad6099756cd63c5
Alex Groß <alex@superschurke.de>
2021-04-23 11:03:59 +0200

    only access to locator as GM

--- dsa5-traveller.js
+++ dsa5-traveller.js
@@ -15,14 +15,15 @@ Hooks.once('ready', function () {
 
 Hooks.on("getSceneControlButtons", (controls) => {
     const meisterPanel = controls.find(c => (c.name === meistertoolsModuleName))
-    meisterPanel['tools'].push({
-        name: "locator",
-        title: 'Locator',
-        icon: "fas fa-street-view",
-        visible: true,
-        button: true,
-        onClick: () => new Dsa5Locations().render(true)
-    })
+    if (game.user.isGM)
+        meisterPanel['tools'].push({
+            name: "locator",
+            title: 'Locator',
+            icon: "fas fa-street-view",
+            visible: true,
+            button: true,
+            onClick: () => new Dsa5Locations().render(true)
+        })
     meisterPanel['tools'].push({
         name: "nightwatch",
         title: 'Nachtwache',
@@ -31,39 +32,6 @@ Hooks.on("getSceneControlButtons", (controls) => {
         button: true,
         onClick: () => new Dsa5Nightwatch().render(true)
     })
-
-/*
-    meisterPanel['tools'].push({
-        name: "draw",
-        title: 'Malen',
-        icon: "fas fa-pen",
-        visible: true,
-        button: true,
-        onClick: () => {
-            Drawing.create({
-                type: CONST.DRAWING_TYPES.RECTANGLE,
-                author: game.user._id,
-                x: 1000,
-                y: 1000,
-                width: 800,
-                height: 600,
-                fillType: CONST.DRAWING_FILL_TYPES.SOLID,
-                fillColor: "#0000FF",
-                fillAlpha: 0.5,
-                strokeWidth: 4,
-                strokeColor: "#FF0000",
-                strokeAlpha: 0.75,
-                texture: "ui/parchment.jpg",
-                textureAlpha: 0.5,
-                text: "HELLO DRAWINGS!",
-                fontSize: 48,
-                textColor: "#00FF00",
-                points: []
-            });
-
-        }
-    })
-*/
 
     controls = controls.filter(c => (c.name !== meistertoolsModuleName))
     controls.push(meisterPanel)
@@ -74,6 +42,7 @@ function registerSettings() {
     const defaultSettings = Dsa5Locations.getDefaultSettings()
 
     game.settings.register(moduleName, "general", {
+        scope: "world",
         config: false,
         type: Object,
         restricted: true,
@@ -81,6 +50,7 @@ function registerSettings() {
     });
 
     game.settings.register(moduleName, "regions", {
+        scope: "world",
         config: false,
         type: Object,
         restricted: true,
@@ -88,6 +58,7 @@ function registerSettings() {
     });
 
     game.settings.register(moduleName, "biomes", {
+        scope: "world",
         config: false,
         type: Object,
         restricted: true,
@@ -95,6 +66,7 @@ function registerSettings() {
     });
 
     game.settings.register(moduleName, "location", {
+        scope: "world",
         config: false,
         type: Object,
         restricted: true,
